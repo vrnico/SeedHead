dfafdd0228d3635893647b7bdf4127eed0f44016
Kayla Ondracek and Nico Daunt <kayla.renee@gmail.com and nico.daunt@gmail.com>
2018-04-24 16:05:19 -0700

    add full CRUD functionality for Seed class

--- SeedHead/Controllers/SeedsController.cs
+++ SeedHead/Controllers/SeedsController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using SeedHead.Models;
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -33,6 +34,35 @@ namespace SeedHead.Controllers
         public IActionResult Create(Seed seed)
         {
             db.Seeds.Add(seed);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var thisSeed = db.Seeds.FirstOrDefault(seeds => seeds.SeedId == id);
+            return View(thisSeed);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Seed seed)
+        {
+            db.Entry(seed).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var thisItem = db.Seeds.FirstOrDefault(seeds => seeds.SeedId == id);
+            return View(thisItem);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var thisSeed = db.Seeds.FirstOrDefault(seeds => seeds.SeedId == id);
+            db.Seeds.Remove(thisSeed);
             db.SaveChanges();
             return RedirectToAction("Index");
         }
