64809e0956becd74adacf1c4d97765d4503a443c
vrnico <nico.daunt@gmail.com>
2018-04-27 10:02:47 -0700

    *crosses fingers*  link test to main project file

---
+++ SeedHead.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SeedHead.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
