a4011bc7e4f5139223139381d31ac6f9ae974b01
vrnico <nico.daunt@gmail.com>
2018-04-27 10:15:35 -0700

    create green test condition for return Seed Description, new error based on netcoreapp incompatibility NU1202?

---
+++ SeedHead.Tests/ModelTests/SeedTests.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SeedHead.Models;
+
+namespace SeedHead.Tests
+{
+    [TestClass]
+    public class SeedTests
+    {
+        [TestMethod]
+        public void GetDescription_ReturnsItemDescription_String()
+        {
+            //Arrange
+            var seed = new Seed();
+
+            seed.Description = "cool";
+
+            //Act
+            var result = seed.Description;
+
+            //Assert
+            Assert.AreEqual("cool", result);
+        }
+    }
+}
