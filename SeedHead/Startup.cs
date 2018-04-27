b15fe1406a3838f8996e978acf6dd93eee716ad1
vrnico <nico.daunt@gmail.com>
2018-04-27 11:14:49 -0700

    static files bug fixed. added reference in startup.cs

--- SeedHead/Startup.cs
+++ SeedHead/Startup.cs
@@ -39,6 +39,7 @@ namespace SeedHead
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
+            app.UseStaticFiles();
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
