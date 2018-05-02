20a283b07ba917dd2c9222edbee7651b9e142ee7
vrnico <nico.daunt@gmail.com>
2018-05-01 19:23:58 -0700

    create green test condition for Review Equals True

---
+++ SeedHead.Tests/ModelTests/ReviewTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SeedHead.Models;
+
+
+namespace SeedHead.Tests
+{
+    [TestClass]
+    public class ReviewTests
+    {
+        [TestMethod]
+        public void Review_Equals_TrueIfSame()
+        {
+            //Arrange  Act
+            Review newReview = new Review("Nico", 5, "Good plant");
+            Review twoReview = new Review("Nico", 5, "Good plant");
+
+            //Assert
+            Assert.AreEqual(newReview, twoReview);
+
+        }
+    }
+}
